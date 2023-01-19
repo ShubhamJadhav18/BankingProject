511504f9c7a243945e353c00fc015be6559e1586
ShubhamJadhav18 <shubham.jadhav1719@gmail.com>
2023-01-19 19:29:26 +0530

    DGTL-101:Done base class changes

--- src/main/java/com/banikngproject/base/BaseClass.java
+++ src/main/java/com/banikngproject/base/BaseClass.java
@@ -1,5 +1,18 @@
 package com.banikngproject.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseClass {
+	public static void main(String[] args) {
+		 System.setProperty("webdriver.chrome.driver","C:\\Users\\ADMIN\\Downloads\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		 
+			WebDriver driver = new ChromeDriver();
+			 driver.get("https://demo.guru99.com/v1/index.php");
+			
+			 
+			 
+		
+	}
 
 }
